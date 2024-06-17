aa9417345d67e289f5b3748aaea1f7728088bdb0
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-06-17 21:30:51 +0530

    Time: 3 ms (94.76%), Space: 8.4 MB (79.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,18 @@
-
 class Solution {
 public:
+    
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) return false;
-
-        unordered_map<char, int> s1Map, s2Map;
-        // Initialize frequency maps for s1 and the first window in s2
-        for (int i = 0; i < s1.length(); ++i) {
-            s1Map[s1[i]]++;
-            s2Map[s2[i]]++;
+        int m[26] = {0}; //character array
+        for(char c : s1) m[c - 'a']++;
+        int j = 0, i = 0, total_chars = s1.size();
+        while(j < s2.size()){
+            if(m[s2.at(j++) - 'a']-- > 0)
+                total_chars--;
+            if(total_chars == 0) return true;
+            //shifting of window
+            if(j - i == s1.size() && m[s2.at(i++) - 'a']++ >= 0)
+                total_chars++;
         }
-
-        int count = 0;
-        // Compare maps and count matches
-        for (int i = 0; i < 26; i++) {
-            if (s1Map['a' + i] == s2Map['a' + i])
-                count++;
-        }
-
-        for (int i = 0; i < s2.length() - s1.length(); ++i) {
-            if (count == 26) return true; // All characters match
-
-            // Update the window
-            char r = s2[i + s1.length()], l = s2[i];
-            if (s1Map[r] == s2Map[r]) count--;
-            s2Map[r]++;
-            if (s1Map[r] == s2Map[r]) count++;
-
-            if (s1Map[l] == s2Map[l]) count--;
-            s2Map[l]--;
-            if (s1Map[l] == s2Map[l]) count++;
-        }
-
-        return count == 26; // Check the last window
+        return false;
     }
 };
