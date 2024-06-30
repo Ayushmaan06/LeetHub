e66914d68ecbbd00da196a1c88255da9c1f9ee78
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-06-30 17:32:22 +0530

    Time: 7 ms (87.29%), Space: 11.9 MB (96.54%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,73 +1,105 @@
-
-
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        vector<vector<string>> result;
-        if (wordSet.find(endWord) == wordSet.end()) return result; // End word not in word list
+        int n=wordList.size();
 
-        unordered_map<string, vector<string>> predecessors;
-        unordered_set<string> visited;
+        unordered_set<string> uset;
+        for(auto const &it:wordList){
+            if(it!=beginWord) uset.insert(it);
+        }
+
         queue<string> q;
         q.push(beginWord);
-        visited.insert(beginWord);
-        bool found = false;
 
-        while (!q.empty() && !found) {
-            int size = q.size();
-            unordered_set<string> localVisited;
-            for (int i = 0; i < size; ++i) {
-                string current = q.front();
+        vector<pair<string,int>> levels;
+        int cnt=0;
+        bool flag=false;
+
+        while(!q.empty()){
+            int sz=q.size();
+        
+            for(int i=0;i<sz;i++){
+                string node=q.front();
                 q.pop();
-                string original = current;
-                for (int j = 0; j < current.size(); ++j) {
-                    char originalChar = current[j];
-                    for (char c = 'a'; c <= 'z'; ++c) {
-                        if (c == originalChar) continue;
-                        current[j] = c;
-                        if (wordSet.find(current) != wordSet.end()) {
-                            if (current == endWord) {
-                                found = true;
-                                predecessors[current].push_back(original);
-                            } else if (visited.find(current) == visited.end()) {
-                                if (localVisited.find(current) == localVisited.end()) {
-                                    q.push(current);
-                                    localVisited.insert(current);
-                                }
-                                predecessors[current].push_back(original);
-                            }
+
+                levels.push_back({node,cnt});
+                
+                if(node==endWord){
+                    flag=true;
+                    break;
+                }
+
+                for(int i=0;i<node.length();i++){
+                    char original=node[i];
+                    for(char j='a';j<='z';j++){
+                        if(original==j) continue;
+                        
+                        node[i]=j;
+                        if(uset.find(node)!=uset.end()){
+                            q.push(node);
+                            uset.erase(node);
                         }
                     }
-                    current[j] = originalChar; // Restore the original character
+                    node[i]=original;
                 }
             }
-            for (const auto& word : localVisited) {
-                visited.insert(word);
-            }
+            if(flag) break;
+            cnt++;
+        }
+        
+        vector<vector<string>> ans;
+
+        if(!flag){
+            return ans;
         }
 
-        if (found) {
-            vector<string> path;
-            dfs(endWord, beginWord, predecessors, path, result);
-        }
+        vector<pair<string,int>>::reverse_iterator it=levels.rbegin();
 
-        return result;
+        vector<string> vec;
+        dfs(endWord,levels,ans,vec,it,cnt,beginWord);
+
+        return ans;
     }
 
 private:
-    void dfs(const string& word, const string& beginWord, unordered_map<string, vector<string>>& predecessors,
-             vector<string>& path, vector<vector<string>>& result) {
-        path.push_back(word);
-        if (word == beginWord) {
-            vector<string> tempPath = path;
-            reverse(tempPath.begin(), tempPath.end());
-            result.push_back(tempPath);
-        } else {
-            for (const string& pred : predecessors[word]) {
-                dfs(pred, beginWord, predecessors, path, result);
-            }
+
+    void dfs(string str,vector<pair<string,int>> &levels,vector<vector<string>> &ans,vector<string> &vec,vector<pair<string,int>>::reverse_iterator it,int cnt,string &beginWord){
+        if(str==beginWord){
+            vec.push_back(str);
+            reverse(vec.begin(),vec.end());
+            ans.push_back(vec);
+            reverse(vec.begin(),vec.end());
+            vec.pop_back();
+
+            return;
         }
-        path.pop_back();
+        
+        while(it!=levels.rend() && it->second==cnt){
+            it++;
+        }
+
+        while(it!=levels.rend() && it->second==cnt-1){
+            if(isPossible(str,it->first)){
+                vec.push_back(str);
+                dfs(it->first,levels,ans,vec,it,cnt-1,beginWord);
+                vec.pop_back();
+            }
+            it++;
+        }
+    }
+
+    bool isPossible(string str1,string str2){
+        int n=str1.length();
+
+        int diff=0;
+        for(int i=0;i<n;i++){
+            if(str1[i]!=str2[i]){
+                diff++;
+            }
+
+            if(diff>1) return false;
+        }
+
+        return true;
     }
 };
