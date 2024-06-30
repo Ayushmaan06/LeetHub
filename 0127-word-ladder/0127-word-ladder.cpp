ba9c9aa7c466396f0e688b8efbe2bea49f73260b
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-06-30 16:19:45 +0530

    Time: 921 ms (16%), Space: 33.6 MB (16.99%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    bool differByOneChange(const string& str1, const string& str2) {
-        // Since both strings have the same length, we only need to check one length
-        int differences = 0;
-        for (int i = 0; i < str1.length(); ++i) {
-            if (str1[i] != str2[i]) {
-                ++differences;
-                if (differences > 1) {
-                    return false; // More than one difference found
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> todo;
+        todo.push(beginWord);
+        int ladder = 1;
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+                string word = todo.front();
+                todo.pop();
+                if (word == endWord) {
+                    return ladder;
                 }
-            }
-        }
-        return differences == 1; // True if exactly one difference, false otherwise
-    }
-
-    int ladderLength(string bw, string ew, vector<string>& wl) {
-        queue<string> q;
-        q.push(bw);
-        int c = 1; // Start with 1 to count the initial word
-        vector<bool> v(wl.size(), false);
-        
-        while (!q.empty()) {
-            int size = q.size(); // Number of elements at the current level
-            for (int k = 0; k < size; ++k) {
-                string current = q.front();
-                q.pop();
-
-                for (int i = 0; i < wl.size(); ++i) {
-                    if (!v[i] && differByOneChange(current, wl[i])) {
-                        if (wl[i] == ew) {
-                            return c + 1; // Return the level count when the end word is found
+                dict.erase(word);
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = 'a' + k;
+                        if (dict.find(word) != dict.end()) {
+                            todo.push(word);
                         }
-                        q.push(wl[i]);
-                        v[i] = true;
-                    }
+                     }
+                    word[j] = c;
                 }
             }
-            ++c; // Increment level count after finishing the current level
+            ladder++;
         }
-        
-        return 0; // Return 0 if no valid transformation sequence is found
+        return 0;
     }
 };
