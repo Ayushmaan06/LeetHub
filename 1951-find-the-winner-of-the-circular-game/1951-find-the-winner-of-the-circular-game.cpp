af168dd2b9ec639a987d1e18595e73bad61c0d88
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-07-08 10:09:21 +0530

    Time: 2 ms (75.45%), Space: 7.2 MB (76.96%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int w = 0;
         for(int i = 1 ; i <= n;i++){
             w=(w+k)%i;
