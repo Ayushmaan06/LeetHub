15b1cd71269642daaec271607ba8e6130d7b77ca
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-07-25 12:07:36 +0530

    Time: 51 ms (99.13%), Space: 70.4 MB (70.15%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+
     vector<int> sortArray(vector<int>& nums) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(NULL);
+        std::cout.tie(NULL);
         sort(nums.begin() , nums.end());
         return nums;
     }
