6547f6001e6109b873694871b61c91ede60cdfc9
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-07-29 15:24:41 +0530

    Time: 31 ms (67.12%), Space: 11.6 MB (51.74%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numTeams(vector<int>& a) {
+        int n = a.size(), c=0;
+        for(int i = 0 ; i < n ; i ++){
+            int sl=0 , lr = 0,ll,sr;
+            for(int j = 0 ; j<n ; j++){
+                if(j<i && a[j]<a[i]){
+                    sl++;
+                }
+                if(j>i && a[j]>a[i]){
+                    lr++;
+                }
+            }
+            ll=i-sl;sr=n-i-1-lr;
+            c+=sl*lr + ll*sr;
+        }
+        return c;
+    }
+};
