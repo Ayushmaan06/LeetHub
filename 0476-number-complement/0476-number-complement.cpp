79bfca2a61d4e082621a751947c36551853b1fb4
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-08-22 20:18:28 +0530

    Time: 3 ms (41.07%), Space: 7.6 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int findComplement(int n) {
-        // Handle the edge case where n is 0
         if (n == 0) return 1;
-
-        // Calculate the bit length of n
         int bitLength = 0;
         int temp = n;
         while (temp > 0) {
@@ -12,10 +9,8 @@ public:
             temp >>= 1;
         }
 
-        // Create a mask with the same number of bits as n, all set to 1
         unsigned int mask = (1U << bitLength) - 1;
 
-        // Flip the bits of n using the mask
-        return n ^ mask;
+        return  mask-n;
     }
 };
