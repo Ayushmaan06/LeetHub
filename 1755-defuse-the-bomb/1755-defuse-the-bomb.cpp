9fbce1cb1cca4e413ea45edc4880a64c4e45b089
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-11-18 16:32:18 +0530

    Time: 0 ms (100%), Space: 10.4 MB (17.93%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -3,32 +3,24 @@ public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
         vector<int> ans(n, 0);
-        bool flag = true;
-
+        
         if (k == 0) {
-            return ans; // All elements are replaced by 0 if k == 0
+            return ans; 
         }
 
-        if (k < 0) {
-            k *= -1;
-            reverse(code.begin(), code.end());
-            flag = false;
+        int start = k > 0 ? 1 : n + k; // Start of the range
+        int end = k > 0 ? k : n - 1;   // End of the range
+        k = abs(k); // Use positive value of k for simplicity
+        
+        int windowSum = 0;
+        for (int i = start; i <= end; i++) {
+            windowSum += code[i % n];
         }
 
-        // Extend the circular array
         for (int i = 0; i < n; i++) {
-            code.push_back(code[i]);
-        }
-
-        // Use the sliding window sum logic
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= k; j++) {
-                ans[i] += code[i + j];
-            }
-        }
-
-        if (!flag) {
-            reverse(ans.begin(), ans.end());
+            ans[i] = windowSum;
+            windowSum -= code[(start++) % n];
+            windowSum += code[(++end) % n];
         }
 
         return ans;
