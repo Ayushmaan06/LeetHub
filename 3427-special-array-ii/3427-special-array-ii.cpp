2e694ce26f8f0c5004838c9920e849148227cf4b
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-12-09 11:31:04 +0530

    Time: 49 ms (17.58%), Space: 135.5 MB (43.73%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,28 +1,45 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<bool> ans;
-        vector<int> prefixHasEvenXor(n + 1, 0);
+    bool binarySearch(int start, int end, vector<int>& violatingIndices) {
+        int left = 0;
+        int right = violatingIndices.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int violatingIndex = violatingIndices[mid];
 
-        // Preprocess to find if there's any even XOR up to each position
-        for (int i = 1; i < n; ++i) {
-            int k = nums[i] ^ nums[i - 1];
-            prefixHasEvenXor[i + 1] = prefixHasEvenXor[i] + ((k & 1) == 0);
+            if (violatingIndex < start) {
+                left = mid + 1;
+            } else if (violatingIndex > end) {
+                right = mid - 1;
+            } else {
+                return true;
+            }
         }
 
-        // Process each query
-        for (const auto& query : queries) {
-            int l = query[0], r = query[1];
-            // Check if there's any even XOR in the range [l + 1, r]
-            if (prefixHasEvenXor[r + 1] - prefixHasEvenXor[l + 1] > 0) {
-                ans.push_back(false);
+        return false;
+    }
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<bool> ans(queries.size());
+        vector<int> violatingIndices;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                violatingIndices.push_back(i);
+            }
+        }
+
+        for (int i = 0; i < queries.size(); i++) {
+            vector<int> query = queries[i];
+            int start = query[0];
+            int end = query[1];
+
+            bool foundViolatingIndex =
+                binarySearch(start + 1, end, violatingIndices);
+
+            if (foundViolatingIndex) {
+                ans[i] = false;
             } else {
-                ans.push_back(true);
+                ans[i] = true;
             }
         }
 
