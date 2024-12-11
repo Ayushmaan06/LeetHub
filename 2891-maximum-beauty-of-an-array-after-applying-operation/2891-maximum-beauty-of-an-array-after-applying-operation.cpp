8e67fee9dbce536fd3c37223c111391ac6567f4c
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-12-11 11:41:48 +0530

    Time: 499 ms (25.38%), Space: 247.9 MB (10.85%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        vector<pair<int,int>> v;
+        // vector<pair<int,int>> v;
         unordered_map<int,int>m;
+    
+        // for(int num : nums){
+        //     v.push_back({num-k,num+k});
+        // }
+        
+        // for(int i = mi;i<=ma;i++){
+        //     m[mi]=0;
+        // }
+        // for(auto it : v){
+        //     m[it.first]+=1;
+        //     m[it.second+1]-=1;
+        // }
+        
+        int mi=INT_MAX,ma=INT_MIN;
         for(int num : nums){
-            v.push_back({num-k,num+k});
+            m[num-k]+=1;
+            m[num+k+1]-=1;
+            mi=min(num,mi);
+            ma=max(num,ma);
         }
-        int mi = *min_element(nums.begin(),nums.end())-k;
-        int ma = *max_element(nums.begin(),nums.end())+k;
-        for(int i = mi;i<=ma;i++){
-            m[mi]=0;
-        }
-        for(auto it : v){
-            m[it.first]+=1;
-            m[it.second+1]-=1;
-        }
+        mi-=k;ma+=k;
         int x=m[mi];
         for(int i = mi+1 ; i<=ma ; i++){
             m[i]+=m[i-1];
             x=max(x,m[i]);
         }
         return x;
+
     }
 };
