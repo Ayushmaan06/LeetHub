760714f47d96c16247777142a1eecd23239c038b
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-02-04 11:19:42 +0530

    Time: 0 ms (100%), Space: 11 MB (77.09%) - LeetHub

---
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int ans = 0, sum = 0;
+        sum = nums[0];
+        ans=sum;
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i]> nums[i-1]){
+                sum+=nums[i];
+            }
+            else{
+                ans=max(ans,sum);
+                sum=nums[i];
+            }
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
