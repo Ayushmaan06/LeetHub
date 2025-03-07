102a1b57abbc978ea4d4791f0b40651b407446b4
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-07 13:49:37 +0530

    Time: 295 ms (34.63%), Space: 48.1 MB (20.93%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,10 +1,7 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
 
 class Solution {
 public:
-    // Function to get all primes up to n using the Sieve of Eratosthenes
+
     vector<int> SieveOfEratosthenes(int n) {
         vector<int> ans;
         vector<bool> prime(n + 1, true);
@@ -17,7 +14,7 @@ public:
             }
         }
 
-        // Collect all primes in the result vector
+
         for (int p = 2; p <= n; p++) {
             if (prime[p]) {
                 ans.push_back(p);
@@ -30,17 +27,14 @@ public:
         // Get all primes up to right
         vector<int> primes = SieveOfEratosthenes(right);
 
-        // Find the index of the first prime >= left
+
         int it = lower_bound(primes.begin(), primes.end(), left) - primes.begin();
         int n = primes.size();
 
-        // If there are no primes in the range, return [-1, -1]
         if (it >= n) return {-1, -1};
 
         int minDiff = INT_MAX;
         vector<int> ans = {-1, -1};
-
-        // Check pairs of consecutive primes
         for (int i = it; i < n - 1; i++) {
             if (primes[i + 1] - primes[i] < minDiff) {
                 minDiff = primes[i + 1] - primes[i];
