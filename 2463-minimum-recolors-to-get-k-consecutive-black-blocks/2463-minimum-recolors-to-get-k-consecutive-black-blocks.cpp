c2b0f56fb8ad4530d29bbdef098a60f12d475a26
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-08 13:02:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (92.08%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -3,19 +3,12 @@ public:
     int minimumRecolors(string blocks, int k) {
         int l = 0, r = 0, n = blocks.length(), ans = INT_MAX, count = 0;
         
-        // Slide the window and maintain the count of 'B' in the current window.
         while (r < n) {
-            // Add the current block to the window
             if (blocks[r] == 'B') {
                 count++;
             }
-            
-            // When we have a full window of size k, calculate the result
             if (r - l + 1 == k) {
-                // Update the answer by the number of 'B's in the current window
                 ans = min(ans, k - count);
-                
-                // Slide the window, remove the leftmost element from the window
                 if (blocks[l] == 'B') {
                     count--;
                 }
