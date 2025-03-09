d9928b2c42050fd7b78698a267e6d7a2826eb007
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-09 14:14:16 +0530

    Time: 75 ms (41.84%), Space: 102.5 MB (91.84%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-    int n = (int)colors.size();
-    vector<int> alt(n), prefix(2*n+1, 0);
-
-    for(int i = 0; i < n; i++){
-        alt[i] = (colors[i] != colors[(i+1) % n]) ? 1 : 0;
+        const int n=colors.size(), sz=n+k-1;
+        int ans=0, alt=1, prev=colors[0];
+        for(int i=1; i<sz; i++){
+            alt=(prev==colors[i%n])?1:alt+1;
+            if (alt>=k) ans++;
+            prev=colors[i%n];
+        }
+        return  ans;
     }
-
-    // Duplicate alt for handling circular wrap
-    for(int i = 0; i < 2*n; i++){
-        prefix[i+1] = prefix[i] + alt[i % n];
-    }
-
-    int count = 0;
-    // Slide over prefix array to see if sum of length (k-1) is (k-1)
-    for(int i = 0; i < n; i++){
-        if(prefix[i + (k-1)] - prefix[i] == (k-1)) count++;
-    }
-    return count;
-}
 };
