74fb54d0db525555e1bf520e00982b74ea19a6cc
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-14 11:42:04 +0530

    Time: 14 ms (90.78%), Space: 88.2 MB (73.33%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,26 +1,21 @@
 class Solution {
-public:
-    bool isP(int m, long long k, vector<int>& vv) {
-        long long j = 0;
-        for(int v : vv) {
-            j += v / m; 
+    bool isPossible(vector<int>& can, long long mid, long long k) {
+        long long total = 0;
+        for(auto x: can) {
+            total += x / mid;
         }
-        return j >= k;
+        return total >= k;
     }
-    
-    int maximumCandies(vector<int>& candies, long long k) {
-        int l = 1, r = *max_element(candies.begin(), candies.end());
-        int ans = 0;
-        
-        while(l <= r) {  
-            int m = l + (r - l) / 2;
-            if(isP(m, k, candies)) {
-                ans = m;  
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
+public:
+    int maximumCandies(vector<int>& can, long long k) {
+        long long n = can.size(), end = 0, start = 1;
+        for(auto x: can) end += x;
+        end /= k;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(isPossible(can, mid, k)) start = mid + 1;
+            else end = mid - 1;
         }
-        return ans;
+        return end;
     }
 };
