aed9d9ab082c5a5f59295169ed9650743253f53d
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-14 11:40:35 +0530

    Time: 37 ms (28.77%), Space: 88.1 MB (97.17%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isP(int m, long long k, vector<int>& vv) {
+        long long j = 0;
+        for(int v : vv) {
+            j += v / m;  // Ensure division is safe
+        }
+        return j >= k;
+    }
+    
+    int maximumCandies(vector<int>& candies, long long k) {
+        int l = 1, r = *max_element(candies.begin(), candies.end());
+        int ans = 0;
+        
+        while(l <= r) {  
+            int m = l + (r - l) / 2;
+            if(isP(m, k, candies)) {
+                ans = m;  
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+};
