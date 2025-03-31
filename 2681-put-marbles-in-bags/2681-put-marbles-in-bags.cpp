df3dc3091e5a453af35b255d8a170eef7d6bea83
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-03-31 11:34:10 +0530

    Time: 38 ms (46.84%), Space: 73 MB (24.47%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        if (k == weights.size()) return 0;
+
+        vector<long long> gaps;
+        for (int i = 0; i < weights.size() - 1; ++i) {
+            gaps.push_back(weights[i] + weights[i + 1]);
+        }
+
+        sort(gaps.begin(), gaps.end());
+
+        long long maxScore = 0;
+        for (int i = gaps.size() - k + 1; i < gaps.size(); ++i) {
+            maxScore += gaps[i];
+        }
+
+        long long minScore = 0;
+        for (int i = 0; i < k - 1; ++i) {
+            minScore += gaps[i];
+        }
+
+        return maxScore - minScore;
+    }
+};
