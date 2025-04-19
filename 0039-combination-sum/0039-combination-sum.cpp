32f925d077c9784005a8388d2e916129f8bda875
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-04-19 14:51:26 +0530

    Time: 0 ms (100%), Space: 14.2 MB (55.68%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,28 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
-public:
-    void solve(vector<int>& o, vector<int>& i, int k, vector<vector<int>>& r, int start, int currentSum) {
-        if(currentSum > k) {
+    public:
+        void solve(vector<int>& in,vector<int>& out,vector<vector<int>>& ans,int i, int sum,int target){
+            if(sum==target){
+                ans.push_back(out);
+                i++;
+                return;
+            }
+            if(sum>target || i >= in.size())return;
+            out.push_back(in[i]);
+            sum+=in[i];
+            solve(in,out,ans,i,sum,target);
+            sum-=in[i];
+            out.pop_back();
+            solve(in,out,ans,i+1,sum,target);
             return;
+            
         }
-        if(currentSum == k) {
-            r.push_back(o);
-            return;
+        vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+            vector<vector<int>> ans;
+            vector<int> out;
+            int n = nums.size();
+            solve(nums,out,ans,0,0,target);
+            return ans;
+
         }
-        for(int j = start; j < i.size(); j++) {
-            // Skip duplicates
-            if(j > start && i[j] == i[j-1]) continue;
-            // Choose
-            o.push_back(i[j]);
-            // Explore
-            solve(o, i, k, r, j, currentSum + i[j]); // j for unlimited usage of the same element
-            o.pop_back();
-        }
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> o;
-        vector<vector<int>> r;
-        sort(candidates.begin(), candidates.end()); // Sort to handle duplicates
-        solve(o, candidates, target, r, 0, 0);
-        return r;
-    }
-};
+    };
+    
