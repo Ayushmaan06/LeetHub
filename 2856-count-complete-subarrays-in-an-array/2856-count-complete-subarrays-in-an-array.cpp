ee908f614f1b9388619ac645ed225cee7dd510de
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-04-24 12:58:04 +0530

    Time: 23 ms (51.27%), Space: 41.9 MB (49.93%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
-        int total = 0;
-        int n = nums.size();
-        unordered_set<int> distinct(nums.begin(), nums.end());
-        int total_unique = distinct.size();
+    int countCompleteSubarrays(vector<int>& nums, int left = 0, int res = 0) {
+        int k = unordered_set<int>(nums.begin(),nums.end()).size();
+        unordered_map<int, int> mpp;
 
-        for (int i = 0; i < n; ++i) {
-            unordered_set<int> seen;
-            for (int j = i; j < n; ++j) {
-                seen.insert(nums[j]);
-                if (seen.size() == total_unique) {
-                    total++;
-                }
+        for (int i = 0; i < nums.size(); ++i) {
+            mpp[nums[i]]++;
+            while (mpp.size() == k) {
+                res += nums.size() - i;  
+                mpp[nums[left]]--;
+                if (mpp[nums[left]] == 0) mpp.erase(nums[left]);
+                left++;
             }
         }
-        return total;
+        return res;
     }
 };
