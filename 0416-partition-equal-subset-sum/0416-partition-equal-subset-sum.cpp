483751c051e458e29196a38149a876254c67480a
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-05-29 12:48:20 +0530

    Time: 155 ms (26.94%), Space: 111.3 MB (14.43%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&dp, vector<int>& arr, int s, int n){
+        if(n==0 || s==0) return 0;
+        if(dp[n][s] != -1) return dp[n][s];
+        int ans = 0;
+        if(arr[n-1] > s)
+            ans = solve(dp, arr, s, n-1);
+        else
+            ans = max(arr[n-1] + solve(dp, arr, s-arr[n-1], n-1), solve(dp, arr, s, n-1));
+        return dp[n][s] = ans;
+    }
+
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        return (solve(dp, arr, sum, n) == sum);
+    }
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if (total % 2 != 0) return false;
-        int sum = total / 2;
-        int n = nums.size();
-        // vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
-
-        // // Initialize: 0 sum is always possible
-        // for (int i = 0; i <= n; i++) dp[i][0] = true;
-
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= sum; j++) {
-        //         if (nums[i - 1] <= j)
-        //             dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-        //         else
-        //             dp[i][j] = dp[i - 1][j];
-        //     }
-        // }
-
-        // return dp[n][sum];
-        vector<bool> dp(sum+1,false);
-        dp[0]=true;
-        for(int num : nums){
-            for(int j = sum ; j>=num ; j--){
-                dp[j]=dp[j]||dp[j-num];
-            }
-        }
-        return dp[sum];
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2)return false;
+        sum/=2;
+        return isSubsetSum(nums,sum);
     }
 };
