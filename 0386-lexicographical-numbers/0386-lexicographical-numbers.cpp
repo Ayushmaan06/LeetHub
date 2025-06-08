31cde8b35f1f769aef588faca668b5cd72fe2551
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-06-08 21:42:19 +0530

    Time: 0 ms (100%), Space: 14 MB (83.88%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> v;
-        int current = 1;
+        int curr = 1;
         
         for (int i = 0; i < n; ++i) {
-            v.push_back(current);
-            if (current * 10 <= n) {
-                current *= 10;
+            v.push_back(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
             } else {
-                if (current >= n) current /= 10;
-                current++;
-                while (current % 10 == 0) current /= 10;
+                if (curr >= n) curr /= 10;
+                curr++;
+                while (curr % 10 == 0) curr /= 10;
             }
         }
         
