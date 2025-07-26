af644e637a946c44d4dcf3a30a3d1b78c9039c90
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-07-26 22:44:43 +0530

    Time: 193 ms (37.41%), Space: 19.8 MB (89.16%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue<string> todo;
-        todo.push(beginWord);
-        int ladder = 1;
-        while (!todo.empty()) {
-            int n = todo.size();
-            for (int i = 0; i < n; i++) {
-                string word = todo.front();
-                todo.pop();
-                if (word == endWord) {
-                    return ladder;
-                }
-                dict.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (dict.find(word) != dict.end()) {
-                            todo.push(word);
-                        }
-                     }
-                    word[j] = c;
-                }
-            }
-            ladder++;
-        }
-        return 0;
-    }
+    bool diff1(const string &s1, const string &s2) {
+        int diffCount = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] != s2[i]) diffCount++;
+            if (diffCount > 1) return false;
+        }
+        return diffCount == 1;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (wordSet.find(endWord) == wordSet.end()) return 0;  
+        queue<string> q;
+        q.push(beginWord);
+
+        int level = 1;
+
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                string current = q.front();
+                q.pop();
+
+                if (current == endWord) return level;
+
+                for (auto it = wordSet.begin(); it != wordSet.end();) {
+                    if (diff1(current, *it)) {
+                        q.push(*it);
+                        it = wordSet.erase(it); 
+                    } else {
+                        ++it;
+                    }
+                }
+            }
+            level++;
+        }
+
+        return 0;
+    }
 };
