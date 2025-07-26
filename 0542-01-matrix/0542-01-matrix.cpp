dbe35f1745b3a629285d0564a9148db35354ceff
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-07-26 14:53:12 +0530

    Time: 23 ms (44.43%), Space: 34.7 MB (87.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        
-        // Push all 0s to the queue and set their distance to 0
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({i, j});
-                    dist[i][j] = 0;
+        int n = mat.size(), m = mat[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j])mat[i][j]=INT_MAX;
+                else q.push({i,j});
+            }
+        }
+        vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        while(!q.empty()){
+            auto[r,c]=q.front();q.pop();
+            for(auto[rr,cc]:dir){
+                int nr=r+rr,nc=c+cc;
+                if(nr>=0 && nc>=0 && nr<n && nc<m && mat[r][c]+1<mat[nr][nc]){
+                    mat[nr][nc]=min(mat[nr][nc],mat[r][c]+1);
+                    q.push({nr,nc});
                 }
             }
         }
-        
-        // Define the four possible directions to move in the matrix
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        // BFS to update distances
-        while (!q.empty()) {
-            auto [row, col] = q.front();
-            q.pop();
-            
-            for (auto [dr, dc] : directions) {
-                int newRow = row + dr, newCol = col + dc;
-                
-                // If new cell is within bounds and can be relaxed
-                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-                    if (dist[newRow][newCol] > dist[row][col] + 1) {
-                        dist[newRow][newCol] = dist[row][col] + 1;
-                        q.push({newRow, newCol});
-                    }
-                }
-            }
-        }
-        
-        return dist;
+        return mat;
     }
 };
