f4403e96891d87df46d6a95f9276a8a983c55d30
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-07-28 18:41:03 +0530

    Time: 2 ms (24.96%), Space: 9.5 MB (82.63%) - LeetHub

--- 1619-path-crossing/1619-path-crossing.cpp
+++ 1619-path-crossing/1619-path-crossing.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        // Custom hash function for pair<int,int>
-        struct pair_hash {
-            size_t operator()(const pair<int, int>& p) const {
-                return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+       int x=0;
+       int y=0;
+       set<array<int, 2>> myset;
+       myset.insert({0,0});
+       for (char chr:path){
+            if(chr=='N'){
+                y+=1;
             }
-        };
+            else if(chr=='S'){
+                y-=1;
+            }
+            else if(chr=='E'){
+                x+=1;
+            }
+            else{
+                x-=1;
+            }
 
-        unordered_set<pair<int, int>, pair_hash> s;
-        pair<int, int> p = {0, 0};
-        s.insert(p); // Start position also needs to be inserted
 
-        for (char c : path) {
-            if (c == 'N') p.second += 1;
-            if (c == 'S') p.second -= 1;
-            if (c == 'E') p.first += 1;
-            if (c == 'W') p.first -= 1;
+            if(myset.find({x,y})!=myset.end()){
+                return true;
+            }
 
-            if (s.count(p)) return true; // Already visited
-            s.insert(p);
+            myset.insert({x,y});
+
         }
-
         return false;
     }
 };
