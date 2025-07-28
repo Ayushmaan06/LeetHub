ed501a5ac2fdaa147ba1bcffe89139a68bc82b63
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-07-28 17:56:59 +0530

    Time: 0 ms (100%), Space: 8 MB (2%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int ans=0;
-        while(n>1){
-            ans+=n/2;
-            
-            if(n%2){
-                n/=2;
-                n++;
-            }
-            else{
-                n/=2;
-            }
-        }
-        return ans;
+        return n-1;
     }
 };
