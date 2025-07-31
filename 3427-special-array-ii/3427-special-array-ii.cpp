924b0d655ae9a8ad48597fba9ae830c5d83d1806
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-07-31 19:43:53 +0530

    Time: 28 ms (39.26%), Space: 126 MB (95.56%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,48 +1,18 @@
 class Solution {
 public:
-    bool binarySearch(int start, int end, vector<int>& violatingIndices) {
-        int left = 0;
-        int right = violatingIndices.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int violatingIndex = violatingIndices[mid];
-
-            if (violatingIndex < start) {
-                left = mid + 1;
-            } else if (violatingIndex > end) {
-                right = mid - 1;
-            } else {
-                return true;
-            }
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& qu) {
+        vector<int> vp;
+        for (int i = 1; i < nums.size(); ++i) {
+            if ((nums[i] & 1) == (nums[i - 1] & 1))
+                vp.push_back(i);          
         }
-
-        return false;
-    }
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<bool> ans(queries.size());
-        vector<int> violatingIndices;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] % 2 == nums[i - 1] % 2) {
-                violatingIndices.push_back(i);
-            }
+        vector<bool> res;
+        for (const auto& q : qu) {
+            int l = q[0], h = q[1];
+            auto first = lower_bound(vp.begin(), vp.end(), l + 1);
+            auto last  = upper_bound(vp.begin(), vp.end(), h);
+            res.push_back(first == last); 
         }
-
-        for (int i = 0; i < queries.size(); i++) {
-            vector<int> query = queries[i];
-            int start = query[0];
-            int end = query[1];
-
-            bool foundViolatingIndex =
-                binarySearch(start + 1, end, violatingIndices);
-
-            if (foundViolatingIndex) {
-                ans[i] = false;
-            } else {
-                ans[i] = true;
-            }
-        }
-
-        return ans;
+        return res;
     }
 };
