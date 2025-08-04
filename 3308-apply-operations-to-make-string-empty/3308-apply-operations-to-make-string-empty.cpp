08edd74947dc94b5991fdd14b2fc6183cd94788f
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-04 15:38:04 +0530

    Time: 20 ms (86.36%), Space: 32.1 MB (92.31%) - LeetHub

--- 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.cpp
+++ 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     string lastNonEmptyString(string s) {
-        unordered_map<char, int> mp;
+        vector<int> v(26,0);
         int ma = INT_MIN;
         for (char c : s) {
-            mp[c]++;
-            ma = max(ma, mp[c]);
+            v[c-'a']++;
+            ma = max(ma, v[c-'a']);
         }
-        for (auto it = mp.begin(); it != mp.end(); ) {
-            it->second -= ma;
-            it->second++;
-            if (it->second <= 0)
-                it = mp.erase(it);
-            else
-                ++it;
+        for(int i =0 ; i < 26 ; i++){
+            if(v[i]){
+                v[i]-=ma;
+                v[i]++;
+                if(v[i]<0)v[i]=0;
+            }
         }
         int n = s.length();
         string res = "";
         for (int i = n - 1; i >= 0; i--) {
-            if (mp[s[i]] > 0) {
-                mp[s[i]]--;
+            if (v[s[i]-'a'] > 0) {
+                v[s[i]-'a']--;
                 res += s[i];
-                if (mp[s[i]] <= 0)
-                    mp.erase(s[i]);
             }
         }
         reverse(res.begin(), res.end());
