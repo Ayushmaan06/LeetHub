9987f9ab843edaeddb9ae3051ebe8b563421c373
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-04 19:02:24 +0530

    Time: 35 ms (36.82%), Space: 98 MB (78.94%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,36 +1,26 @@
 class ProductOfNumbers {
-private:
-    // Stores cumulative product of the stream
-    vector<int> prefixProduct;
-    int size = 0;
-
 public:
+    vector<int> p;
+    int lzi=-1;
+
     ProductOfNumbers() {
-        // Initialize the product list with 1 to handle multiplication logic
-        prefixProduct.push_back(1);
-        size = 0;
+        p.push_back(1); 
     }
 
     void add(int num) {
         if (num == 0) {
-            // If num is 0, reset the cumulative products since multiplication
-            // with 0 invalidates previous products
-            prefixProduct = {1};
-            size = 0;
+            p.clear();
+            p.push_back(1);
+            lzi = p.size();
         } else {
-            // Append the cumulative product of the current number with the last
-            // product
-            prefixProduct.push_back(prefixProduct[size] * num);
-            size++;
+            int last = p.back();
+            p.push_back(last * num);
         }
     }
 
     int getProduct(int k) {
-        // Check if the requested product length exceeds the size of the valid
-        // product list
-        if (k > size) return 0;
-
-        // Compute the product of the last k elements using division
-        return prefixProduct[size] / prefixProduct[size - k];
+        int n = p.size();
+        if (k >= n) return 0;
+        return p[n - 1] / p[n - 1 - k];
     }
 };
