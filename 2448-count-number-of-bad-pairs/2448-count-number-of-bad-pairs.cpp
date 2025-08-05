ed96510d9540a700a40c5a8d496db5e55d072b64
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-05 23:21:26 +0530

    Time: 73 ms (44.04%), Space: 87.7 MB (53.25%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    long long C2(int m) {
-        long long n = m;
-        if (n < 2) {
-            return 0;  // Not valid if n < 2
-        }
-        return (n * (n - 1)) / 2;  // Formula for combinations C(n, 2)
-    }
     long long countBadPairs(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for(int i = 0 ; i < nums.size() ; i++){
-            m[nums[i]-i]++;
+        unordered_map<int,int> diff;
+        int i = 0;
+        long long gp=0;
+        for(int n : nums){
+            gp+=diff[n-i];
+            diff[n-i]++;
+            i++;
         }
-        if(m.size()==1)return 0;
-        long long s=0;
-        for(auto it : m){
-            s+= C2(it.second);
-        }
-        long long t = C2(nums.size());
-        return t-s;
+        long long n = nums.size();
+        long long tp = n*(n-1)/2;
+        return tp-gp;
+
     }
 };
