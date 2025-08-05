e732dcda27483736244cf1f082acc8d893c75ca5
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-05 23:03:51 +0530

    Time: 16 ms (81.36%), Space: 138.5 MB (79.7%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int numRows = mat.size(), numCols = mat[0].size();
-        vector<int> rowCount(numRows), colCount(numCols);
-        unordered_map<int, pair<int, int>> numToPos;
-
-        // Create a map to store the position of each number in the matrix
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                int value = mat[row][col];
-                numToPos[value] = {row, col};
+        long long n=mat.size(),m=mat[0].size();
+        vector<long long> r(n,0),c(m,0);
+        vector<pair<long long,long long>>g(m*n);
+        for(long long i = 0 ; i < n ; i++){
+            for(long long j=0;j<m;j++){
+                r[i]+=mat[i][j];
+                c[j]+=mat[i][j];
+                g[mat[i][j]-1]={i,j};
             }
         }
-
-        for (int i = 0; i < arr.size(); i++) {
-            int num = arr[i];
-            auto [row, col] = numToPos[num];
-
-            // Increment the count for the corresponding row and column
-            rowCount[row]++;
-            colCount[col]++;
-
-            // Check if the row or column is completely painted
-            if (rowCount[row] == numCols || colCount[col] == numRows) {
-                return i;
-            }
+        for(long long i = 0 ; i < arr.size();i++){
+            auto[rr,cc]=g[arr[i]-1];
+            r[rr]-=arr[i];
+            c[cc]-=arr[i];
+            if(r[rr]==0 || c[cc]==0)return i;
         }
-
-        return -1;  // This line will never be reached as per the problem
-                    // statement
+        return -1;
     }
 };
