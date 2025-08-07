d48eb97ed8234e36cde431738d22e3adc5e79602
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-07 15:07:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (22.28%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,15 +1,19 @@
- class Solution {
+class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if (s1 == s2) return true;
-        if (s1.length() != s2.length()) return false;
-        vector<int> diff;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1[i] != s2[i]) {
-                diff.push_back(i);
-            }
+        unordered_map<char,int> mp;
+        for(char c : s1)mp[c]++;
+        for(char c : s2){
+            mp[c]--;
+            if(mp[c]==0)mp.erase(c);
         }
-        if (diff.size() != 2) return false;
-        return s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+        if(!mp.empty())return false;
+        int n1=s1.length(),n2=s2.length();
+        if(n1!=n2)return false;
+        int n = n1,c=0;
+        for(int i = 0 ; i < n ; i++){
+            if(s1[i]!=s2[i])c++;
+        }
+        return c<3;
     }
 };
