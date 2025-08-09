825768ead9cecd0e215e0ace064a7a8a0971d97b
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-09 21:17:58 +0530

    Time: 103 ms (26.43%), Space: 99 MB (55.49%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        // sort(nums.begin() , nums.end());
-        // int it = lower_bound(nums.begin(),nums.end(),k)-nums.begin();
-        priority_queue <long long, vector<long long>, greater<long long>> pq;
-        for(int num : nums){
-            pq.push(num);
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for (int t : nums) {
+            pq.push(t);
         }
-        int c=0;
-        while(pq.top()<k){
-            long long t1=pq.top();
-            pq.pop();
-            long long t2=pq.top();
-            pq.pop();
-            long long w = min(t1,t2)*2 + max(t1,t2);
-            pq.push(w);
-            c++;
+        int ans = 0;
+        while (pq.size() > 1 && pq.top() < k) {
+            long long x = pq.top(); pq.pop();
+            long long y = pq.top(); pq.pop();
+            long long combined = 2LL * x + y;  // Prevent int overflow
+            pq.push(combined);
+            ans++;
         }
-        return c;
+        if (pq.top() < k) return -1;
+
+        return ans;
     }
 };
