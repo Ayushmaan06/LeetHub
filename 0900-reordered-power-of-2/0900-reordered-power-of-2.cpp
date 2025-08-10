91fd9848f3625e63a9f864606068c7931027bcc3
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-10 10:27:48 +0530

    Time: 0 ms (100%), Space: 9 MB (62.83%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        vector<int> v;
-        while(n){
-            v.push_back(n%10);
-            n/=10;
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        for (int i = 0; i < 31; ++i) {               
+            string p = to_string(1 << i);
+            sort(p.begin(), p.end());
+            if (s == p) return true;
         }
-        sort(v.begin(),v.end());
-        do {
-            if(v[0]){
-                int t=0;
-                for(int i = 0 ; i < v.size() ; i++)t=t*10+v[i];
-                if (t > 0 && (t & (t - 1)) == 0) return true;
-            }
-        } while (next_permutation(v.begin(), v.end()));
         return false;
     }
 };
