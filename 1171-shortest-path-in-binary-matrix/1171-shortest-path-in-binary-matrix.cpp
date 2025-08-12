ff9ae6f21a6f36fb0792f377f595b7a8e8e8673a
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-12 21:16:14 +0530

    Time: 7 ms (97.39%), Space: 23.4 MB (92.03%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1; // Start or end is blocked
-        if(n == 1) return 1; // Only one cell in the grid
-
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1; 
+        if(n == 1) return 1; 
         vector<pair<int, int>> directions = {
             {0, 1}, {0, -1}, {1, 0}, {-1, 0},
             {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
         };
-
         queue<pair<int, int>> q;
         q.push({0, 0});
-        grid[0][0] = 1; // Mark as visited by setting to 1
+        grid[0][0] = 1; 
         int pathLength = 1;
 
         while(!q.empty()) {
@@ -20,20 +18,17 @@ public:
             for(int i = 0; i < size; ++i) {
                 auto [row, col] = q.front();
                 q.pop();
-
-                if(row == n-1 && col == n-1) return pathLength; // Reached destination
-
+                if(row == n-1 && col == n-1) return pathLength; 
                 for(const auto& [dr, dc] : directions) {
                     int newRow = row + dr, newCol = col + dc;
                     if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && grid[newRow][newCol] == 0) {
                         q.push({newRow, newCol});
-                        grid[newRow][newCol] = 1; // Mark as visited
+                        grid[newRow][newCol] = 1; 
                     }
                 }
             }
             ++pathLength;
         }
-
-        return -1; // Path not found
+        return -1; 
     }
 };
