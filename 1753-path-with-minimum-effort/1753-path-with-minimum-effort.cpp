78f00d55429c3f1bd1d94e7ce6102c7717ef44fe
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-14 00:16:48 +0530

    Time: 38 ms (93.63%), Space: 24.4 MB (75.53%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
+vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     int minimumEffortPath(vector<vector<int>>& heights) {
-         priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
-        int n = heights.size();
-        int m = heights[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        dist[0][0] = 0;
-        pq.push({0, {0, 0}});
-        vector<pair<int,int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<int>> res(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        res[0][0]=0;
+        pq.push({0,{0,0}});
         while(!pq.empty()){
-            int row = pq.top().second.first, col = pq.top().second.second , d = pq.top().first;
-            pq.pop();
-            if (row == n - 1 && col == m - 1)
-                return d;
-            for (auto [dr, dc] : directions) {
-                int newRow = row + dr, newCol = col + dc;
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m) {
-                    int newEffort = max(abs(heights[row][col] - heights[newRow][newCol]), d);
-
-                    // If the calculated effort is less than the prev value
-                    // we update as we need the min effort.
-                    if (newEffort < dist[newRow][newCol])
-                    {
-                        dist[newRow][newCol] = newEffort;
-                        pq.push({newEffort, {newRow, newCol}});
+            int w = pq.top().first,r=pq.top().second.first,c=pq.top().second.second;pq.pop();
+            if(r==n-1 && c==m-1) return w;
+            if(w>res[r][c]) continue;
+            for(auto[rr,cc] : dir){
+                int nr=rr+r,nc=cc+c;
+                if(nr>=0 && nc>=0 && nr<n && nc< m){
+                    int t=max(w,abs(heights[nr][nc]-heights[r][c]));
+                    if(t<res[nr][nc]){
+                        res[nr][nc]=t;
+                        pq.push({t,{nr,nc}});
                     }
                 }
             }
         }
-        return 0;
+        return res[n-1][m-1];
     }
 };
