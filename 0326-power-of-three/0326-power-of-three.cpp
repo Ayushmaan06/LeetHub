6615ce231683c5bd916af0767ac5ab4b04d72573
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-13 09:34:00 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.26%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    double log_a_to_base_b(double a, double b)
-    {
-        return log2(a) / log2(b);
-    }
+    // double log_a_to_base_b(double a, double b)
+    // {
+    //     return log2(a) / log2(b);
+    // }
     bool isPowerOfThree(int n) {
         if (n <= 0) return false;
-        double x = log_a_to_base_b(n, 3);
-        const double eps = 1e-10;
-        return abs(x - round(x)) < eps;
+        return (1162261467%n == 0);
     }
 };
