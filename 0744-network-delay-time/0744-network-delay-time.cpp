0bda88d4ebd388b9e6c5a51fee22b1b76a9080ac
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-14 16:40:25 +0530

    Time: 72 ms (93.55%), Space: 44.4 MB (69.98%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -3,21 +3,16 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> graph(n + 1);
         for (const auto& time : times) {
-            // Adjust for 1-indexed node numbering
             graph[time[0]].emplace_back(time[1], time[2]);
         }
-
-        // Use greater<> to make it a min-heap
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         vector<int> dist(n + 1, INT_MAX);
         dist[k] = 0;
         pq.push({0, k});
-
         while (!pq.empty()) {
             auto [dis, src] = pq.top();
             pq.pop();
-            if (dis > dist[src]) continue; // Check for outdated distance
-
+            if (dis > dist[src]) continue; 
             for (auto [dst, p] : graph[src]) {
                 if (dist[dst] > dis + p) {
                     dist[dst] = dis + p;
@@ -25,8 +20,7 @@ public:
                 }
             }
         }
-
-        int maxTime = *max_element(dist.begin() + 1, dist.end()); // Skip index 0
+        int maxTime = *max_element(dist.begin() + 1, dist.end()); 
         return maxTime == INT_MAX ? -1 : maxTime;
     }
 };
