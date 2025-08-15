402a8c7eb380be7ddd3e5ef21413d9548a38ff61
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-15 16:34:26 +0530

    Time: 7 ms (61.6%), Space: 13.9 MB (53.57%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,42 +1,27 @@
-#include <vector>
-#include <queue>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         int n = grid.size();
-        // Define a min-heap priority queue where the pair is <height, <row, col>>
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        vector<vector<bool>> visited(n, vector<bool>(n, false)); // To keep track of visited cells
-
-        pq.push({grid[0][0], {0, 0}});
-        visited[0][0] = true; // Mark the starting cell as visited
-
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;
+        pq.push({grid[0][0], 0, 0});
+        visited[0][0] = true;
+        vector<pair<int,int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
         while (!pq.empty()) {
-            auto current = pq.top();
+            auto [currMax, r, c] = pq.top();
             pq.pop();
-            int ht = current.first, row = current.second.first, col = current.second.second;
-
-            if (row == n - 1 && col == n - 1) return ht; // Reached the bottom-right corner
-
-            for (auto& [dr, dc] : directions) {
-                int newRow = row + dr, newCol = col + dc;
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && !visited[newRow][newCol]) {
-                    visited[newRow][newCol] = true; // Mark as visited
-                    int newHt = max(grid[newRow][newCol], ht);
-                    pq.push({newHt, {newRow, newCol}});
+            if (r == n - 1 && c == n - 1)
+                return currMax;
+            for (auto [dr, dc] : dir) {
+                int nr = r + dr, nc = c + dc;
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    int nextMax = max(currMax, grid[nr][nc]);
+                    pq.push({nextMax, nr, nc});
                 }
             }
         }
-
-        return -1; // Should never reach here if the grid is valid
+        return -1; 
     }
 };
