bd4f56fd6cd4374a7e410e141ac6f5a74554f0be
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-18 19:00:13 +0530

    Time: 0 ms (100%), Space: 8.3 MB (98.26%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,32 @@
+class Solution 
+{  
+private:  
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
+    }
+    
+public:
+    int numSquares(int n) 
+    {  
+        // If n is a perfect square, return 1.
+        if(is_square(n)) 
+            return 1;  
+        // The result is 4 if and only if n can be written in the 
+        // form of 4^k*(8*m + 7). Please refer to 
+        // Legendre's three-square theorem.
+        while(n % 4 == 0) n = n / 4;
+        if(n % 8 == 7) return 4;
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++)
+        {  
+            if (is_square(n - i*i)) 
+            {
+                return 2;  
+            }
+        }  
+        
+        return 3;  
+    }  
+}; 
