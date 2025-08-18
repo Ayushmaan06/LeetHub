d6233660e614e3b69d5a928dfba748dd8c481bcd
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-18 18:36:47 +0530

    Time: 0 ms (100%), Space: 11.9 MB (59.5%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,18 +1,9 @@
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& eq, vector<double>& values, vector<vector<string>>& queries) {
         int n = values.size();
         unordered_set<string> is;
         unordered_map<string, vector<pair<string, double>>> mp;
-        // Build the graph
         for (int i = 0; i < n; i++) {
             string u = eq[i][0], v = eq[i][1];
             mp[u].push_back({v, values[i]});
@@ -22,15 +13,12 @@ public:
         }
         
         vector<double> res;
-        // Process each query
         for (vector<string> q : queries) {
-            // q[0] is numerator and q[1] is denominator
             string st = q[0], en = q[1];
             if (is.find(st) == is.end() || is.find(en) == is.end()) {
                 res.push_back(-1.0);
                 continue;
             }
-            // Use a BFS/DFS style search with multiplication of weights
             queue<pair<string, double>> qu;
             qu.push({st, 1.0});
             unordered_set<string> visited;
