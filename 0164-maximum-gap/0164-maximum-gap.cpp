f05ff71f6aa4056f7d9f1dae1c6669207a3f318a
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-08-29 00:24:55 +0530

    Time: 43 ms (53.92%), Space: 105.5 MB (8.52%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void radixSort(vector<int>& arr) {
+        if (arr.empty()) return;
+        long long mx = *max_element(arr.begin(), arr.end());  
+        for (long long exp = 1; mx / exp > 0; exp *= 10) {    
+            int n = arr.size();
+            vector<int> output(n);
+            int count[10] = {0};
+
+            for (int i = 0; i < n; i++) {
+                int digit = (arr[i] / exp) % 10;
+                count[digit]++;
+            }
+
+            for (int i = 1; i < 10; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (arr[i] / exp) % 10;
+                output[count[digit] - 1] = arr[i];
+                count[digit]--;
+            }
+
+            arr = output;
+        }
+    }
+
+
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return 0;
+        radixSort(nums);
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            ans = max(ans, nums[i] - nums[i - 1]);
+        }
+        return ans;
+    }
+};
