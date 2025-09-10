d2e281689b0abf2663c6995a314dedb8cc44c6af
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-10 11:28:52 +0530

    Time: 530 ms (5.88%), Space: 255.2 MB (7.06%) - LeetHub

---
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        unordered_map<int,int> l;
+        unordered_set<int> needTeach;  
+        for(vector<int> f : friendships){
+            int u = f[0], v = f[1];
+            bool com = false;
+            unordered_map<int,int> ll;
+            unordered_set<int> s;
+            for(int x : languages[u-1]){
+                ll[x]++;
+                s.insert(x);
+            }
+            for(int x : languages[v-1]){
+                ll[x]++;
+                if(ll[x]>1){ com = true; break; }
+                s.insert(x);
+            }
+            if(!com){
+                needTeach.insert(u);
+                needTeach.insert(v);
+            }
+        }
+        if(needTeach.empty()) return 0;
+        for(int person : needTeach){
+            for(int lang : languages[person-1]){
+                l[lang]++;
+            }
+        }
+        int ans = -1, m = 0;
+        for(auto it : l){
+            if(it.second > m){
+                m = it.second;
+                ans = it.first;
+            }
+        }
+        return needTeach.size() - m;
+    }
+};
