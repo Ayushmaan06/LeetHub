2a6a2eda16eb4f3c527a3db45d8f970267e916ef
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-17 09:54:58 +0530

    Time: 123 ms (80.61%), Space: 174.2 MB (37.12%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -0,0 +1,34 @@
+class FoodRatings {
+public:
+    struct Comparator {
+        bool operator()(const pair<int,string>& a, const pair<int,string>& b) const {
+            if (a.first == b.first) return a.second < b.second;
+            return a.first > b.first; 
+        }
+    };
+
+    unordered_map<string,int> rating;             // food -> rating
+    unordered_map<string,string> foodCuisine;     // food -> cuisine
+    unordered_map<string,set<pair<int,string>,Comparator>> cuisineFoods; 
+
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for (int i = 0; i < n; i++) {
+            rating[foods[i]] = ratings[i];
+            foodCuisine[foods[i]] = cuisines[i];
+            cuisineFoods[cuisines[i]].insert({ratings[i], foods[i]});
+        }
+    }
+
+    void changeRating(string food, int newRating) {
+        string cuisine = foodCuisine[food];
+        int oldRating = rating[food];
+        cuisineFoods[cuisine].erase({oldRating, food});
+        cuisineFoods[cuisine].insert({newRating, food});
+        rating[food] = newRating;
+    }
+
+    string highestRated(string cuisine) {
+        return cuisineFoods[cuisine].begin()->second;
+    }
+};
