a4709e8284294b39661ef28d23c0ea77efbf7ac7
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-18 16:59:21 +0530

    Time: 22 ms (10.38%), Space: 19.9 MB (16.55%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& st, vector<vector<int>>& g, vector<bool> vis, vector<int> v, int n) {
+        if (v.back() == n - 1) {
+            st.push_back(v);
+            return;
+        }
+        int x = v.back();
+        vis[x] = true;
+        for (int i : g[x]) {
+            if (!vis[i]) {
+                v.push_back(i);
+                solve(st, g, vis, v, n);
+                v.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> st; 
+        vector<bool> vis(n, false); 
+        vector<int> v = {0}; 
+        solve(st, graph, vis, v, n);
+        // vector<vector<int>> result(st.begin(), st.end());
+        return st;
+    }
+};
