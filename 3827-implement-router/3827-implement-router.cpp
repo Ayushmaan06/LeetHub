6de6a3593655583ba8957b69a6d19132241fa717
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-20 11:20:47 +0530

    Time: 271 ms (66.56%), Space: 440.7 MB (39.53%) - LeetHub

---
+++ 3827-implement-router/3827-implement-router.cpp
@@ -0,0 +1,50 @@
+
+struct PairHash {
+    size_t operator()(const pair<int,int>& p) const noexcept {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+
+class Router {
+public:
+    unordered_map<int,unordered_set<pair<int,int>,PairHash>> dst;   
+    unordered_map<int, vector<int>> times;    
+    unordered_map<int,int> remd;                    
+    int x;
+    queue<vector<int>> q;
+
+    Router(int memoryLimit) {
+        x = memoryLimit;
+    }
+    
+    bool addPacket(int source, int destination, int timestamp) {
+        if(dst[destination].count({source,timestamp})) return false;
+
+        if((int)q.size() >= x){
+            auto old = q.front(); q.pop();
+            dst[old[1]].erase({old[0], old[2]});
+            remd[old[1]]++;
+        }
+
+        vector<int> t{source, destination, timestamp};
+        q.push(t);
+        dst[destination].insert({source,timestamp});
+        times[destination].push_back(timestamp);
+        return true;
+    }
+    
+    vector<int> forwardPacket() {
+        if(q.empty()) return {};
+        auto t = q.front(); q.pop();
+        dst[t[1]].erase({t[0],t[2]});
+        remd[t[1]]++;
+        return t;
+    }
+    
+    int getCount(int d, int st, int et) {
+        int t = remd[d];
+        auto it = lower_bound(times[d].begin()+t,times[d].end(),st);
+        auto it1 = upper_bound(times[d].begin()+t,times[d].end(),et);
+        return int(it1-it);
+    }
+};
