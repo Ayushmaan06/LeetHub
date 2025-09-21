de9a40ec4285b859cc57fea4ced36c7fee5fad43
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-21 16:28:58 +0530

    Time: 6 ms (30.83%), Space: 32.2 MB (15.77%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,24 @@
+class BSTIterator {
+public:
+    vector<int> v;
+    int i = -1;
+
+    void ino(TreeNode* r) {
+        if (!r) return;
+        ino(r->left);
+        v.push_back(r->val);
+        ino(r->right);
+    }
+
+    BSTIterator(TreeNode* root) {
+        ino(root); 
+    }
+
+    int next() {
+        return v[++i];
+    }
+
+    bool hasNext() {
+        return (i + 1 < v.size());
+    }
+};
