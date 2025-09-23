0ea7a149426c8e0741dd43ccf10059f14c6cde5d
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-09-23 23:37:58 +0530

    Time: 17 ms (9.61%), Space: 275.6 MB (17.18%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -4,19 +4,11 @@ public:
         int n = derived.size();
         vector<int> og(n,0);
         for(int i = 0 ; i < n-1 ; i++){
-            if(derived[i]==0){
+            if(derived[i])og[i+1]=og[i]^1;
+            else{
                 og[i+1]=og[i];
             }
-            else{
-                og[i+1]=og[i]^1;
-            }
         }
-        if(derived[n-1]==1){
-            if(og[n-1]==og[0])return false;
-        }
-        else{
-            if(og[n-1]!=og[0])return false;
-        }
-        return true;
+        return ((og[n-1]^og[0])==derived[n-1]);
     }
 };
