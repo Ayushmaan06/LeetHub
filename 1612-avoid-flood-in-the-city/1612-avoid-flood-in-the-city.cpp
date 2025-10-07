53e8940f6ba886dcec84875875f5f2a032d404f4
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-10-07 09:54:10 +0530

    Time: 108 ms (85.88%), Space: 114.6 MB (72.6%) - LeetHub

---
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        unordered_map<int, int> full; 
+        set<int> dryDays;
+        int n = rains.size();
+        vector<int> res(n, -1);
+
+        for (int i = 0; i < n; ++i) {
+            if (rains[i] > 0) {
+                int lake = rains[i];
+                if (full.find(lake) != full.end()) {
+                    auto it = dryDays.lower_bound(full[lake]);
+                    if (it == dryDays.end()) return {}; 
+                    res[*it] = lake; 
+                    dryDays.erase(it);
+                }
+                full[lake] = i;
+                res[i] = -1;
+            } else {
+                dryDays.insert(i);
+                res[i] = 1; 
+            }
+        }
+
+        return res;
+    }
+};
