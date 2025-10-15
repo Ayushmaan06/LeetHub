89eeac1f1fd67a820bdd42bbe780bbf8589a6039
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-10-15 20:41:10 +0530

    Time: 187 ms (32.93%), Space: 100.3 MB (5.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,22 @@
 class Solution {
-private:
-    int fun(int idx, int sum, vector<int>& arr, vector<vector<int>>& dp, int n) {
-        if (sum < 0) return 0; // Sum should not be negative
-        if (idx == 0) {
-            if (sum == 0 && arr[0] != 0) return 1;
-            if (sum == 0 && arr[0] == 0) return 2; // If the first element is zero, it can be included or not included, hence two ways
-            return (arr[0] == sum); // If the first element is equal to the sum
-        }
-
-        if (dp[idx][sum] != -1) return dp[idx][sum];
-
-        int l = 0;
-        if (arr[idx] <= sum) l = fun(idx - 1, sum - arr[idx], arr, dp, n); // Take the current element
-
-        int r = fun(idx - 1, sum, arr, dp, n); // Do not take the current element
-
-        return dp[idx][sum] = (l + r) % 1000000007; // Store and return the result
-    }
 public:
-    int findTargetSumWays(vector<int>& nums, int t) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if ((sum - t) % 2 != 0 || sum < t) return 0;
-        int target = (sum - t) / 2;
+    int solve(vector<int> v, int x ,vector<vector<int>>& dp, int n){
+        if(n==0){
+            if(x==0)return 1;
+            else return 0;
+        }
+        if(dp[n][x]!=-1)return dp[n][x];
+        if(v[n-1]>x) dp[n][x]=solve(v,x,dp,n-1);
+        else dp[n][x]=solve(v,x,dp,n-1)+solve(v,x-v[n-1],dp,n-1);
+        return dp[n][x];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int s = accumulate(nums.begin(),nums.end(),0);
+        if((s-target)<0 || (s-target)%2)return 0;
+        int x = s-target;
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        return fun(n - 1, target, nums, dp, n);
+        vector<vector<int>> dp(n+1,vector<int>(x/2 + 1,-1));
+        int c = solve(nums,x/2,dp,n);
+        return c;
     }
 };
