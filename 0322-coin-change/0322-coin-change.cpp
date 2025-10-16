527804078ca28ff8fce8a9ad3130ccda8c0182ae
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-10-16 16:24:23 +0530

    Time: 43 ms (43.73%), Space: 52.8 MB (17.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; ++i) {
-            for (int coin : coins) {
-                if (i - coin >= 0) dp[i] = min(dp[i], 1 + dp[i - coin]);
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX - 1));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[1][j] = j / coins[0];
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+
+        int result = dp[n][amount];
+        return (result >= INT_MAX - 1) ? -1 : result;
     }
 };
