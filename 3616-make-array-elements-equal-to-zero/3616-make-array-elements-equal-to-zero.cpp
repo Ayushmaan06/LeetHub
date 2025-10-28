65270cc7ab27881a7e209b46f5e32f8ad8917d9d
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-10-28 09:04:40 +0530

    Time: 0 ms (100%), Space: 21.8 MB (94.04%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    static int countValidSelections(vector<int>& nums) {
-        const int n=nums.size();
-        vector<int> zIdx;
-        if (nums[0]==0) zIdx.push_back(0);
-        for(int i=1; i<n; i++){
-            if(nums[i]==0) zIdx.push_back(i);
-            nums[i]+=nums[i-1];
+    int countValidSelections(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0LL), ls = 0, cases = 0;
+        for (int num : nums) {
+            int rs = total - ls - num;
+            if (num == 0) {
+                if (ls == rs) {
+                    cases += 2;
+                } else if (ls == rs - 1 || ls - 1 == rs) {
+                    cases++;
+                }
+            }
+            ls += num;
         }
-        int cnt=0, sum=nums[n-1];
-        for (int z: zIdx){
-            if (2*nums[z]==sum) cnt+=2;
-            if (abs(2*nums[z]-sum)==1) cnt++;
-        }
-        return cnt;
+        return cases;
     }
 };
