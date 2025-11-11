90c3cac9adaec620405d584e7cda74b645f86ee8
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-11-11 11:58:11 +0530

    Time: 145 ms (54.37%), Space: 105.9 MB (39.01%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,21 +1,29 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (auto& s : strs) {
-            int zeros = count(s.begin(), s.end(), '0');
-            int ones = s.size() - zeros;
-            for (int i = m; i >= zeros; --i) {
-                for (int j = n; j >= ones; --j) {
-                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
-                }
-            }
+    vector<vector<vector<int>>> dp;
+    vector<pair<int, int>> count; 
+    int l; 
+    int dfs(int idx, int m_left, int n_left) {
+        if (idx == l) return 0;
+        if (dp[idx][m_left][n_left] != -1) return dp[idx][m_left][n_left];
+        int res = dfs(idx + 1, m_left, n_left);
+        int zeros = count[idx].first;
+        int ones = count[idx].second;
+        if (m_left >= zeros && n_left >= ones) {
+            res = max(res, 1 + dfs(idx + 1, m_left - zeros, n_left - ones));
         }
-        return dp[m][n];
+        return dp[idx][m_left][n_left] = res;
+    }
+
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        l = strs.size();
+        count.resize(l);
+        for (int i = 0; i < l; ++i) {
+            int zeros = count_if(strs[i].begin(), strs[i].end(), [](char c){ return c == '0'; });
+            int ones = strs[i].size() - zeros;
+            count[i] = {zeros, ones};
+        }
+        dp.assign(l, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+        return dfs(0, m, n);
     }
 };
