f68a812232755ed3cd61c3ad51351e5f6307ceff
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-12-17 00:55:07 +0530

    Time: 7 ms (93.23%), Space: 12.8 MB (19.08%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> dp(nums.size(), 1), prev(nums.size(), -1);
-        int maxi = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+    vector<int> largestDivisibleSubset(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end()); 
+        vector<int> dp(n, 1);     // dp[i] = max length ending at i
+        vector<int> hash(n);      // hash[i] = index of previous element
+        int maxi = 1;             // Length of the longest subset found
+        int lastIndex = 0;        // The ending index of that subset
+        for(int i=0; i<n; i++) hash[i] = i;
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if (arr[i] % arr[prev] == 0 && 1 + dp[prev] > dp[i]) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev; 
                 }
             }
-            if (dp[i] > dp[maxi]) maxi = i;
+            if (dp[i] > maxi) {
+                maxi = dp[i];
+                lastIndex = i;
+            }
         }
-        vector<int> res;
-        for (int i = maxi; i >= 0; i = prev[i]) {
-            res.push_back(nums[i]);
-            if (prev[i] == -1) break;
+        vector<int> temp;
+        temp.push_back(arr[lastIndex]);
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            temp.push_back(arr[lastIndex]);
         }
-        return res;
+        return temp;
     }
 };
