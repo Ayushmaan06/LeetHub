79f2b1f53e0175a290624c68feedd0853a96e27d
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2025-12-25 22:49:19 +0530

    Time: 5 ms (29.22%), Space: 15.1 MB (20.53%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    string simplifyPath(string path) {
-    vector<string> stack;
-    stringstream ss(path);
-    string temp;
+    string simplifyPath(string p) {
+        int n = p.length();
+        stack<string> st;
+        int i = 0;
+        while (i < n) {
+            while (i < n && p[i] == '/') i++;   
+            if (i >= n) break;
+            int j = i;
+            while (j < n && p[j] != '/') j++;
+            string dir = p.substr(i, j - i);
+            if (dir == "..") {
+                if (!st.empty()) st.pop();
+            }
+            else if (dir != ".") {
+                st.push(dir);
+            }
 
-    while (getline(ss, temp, '/')) {
-        if (temp == "" || temp == ".") continue;
-        if (temp == ".." && !stack.empty()) stack.pop_back();
-        else if (temp != "..") stack.push_back(temp);
+            i = j;
+        }
+        string ans;
+        while (!st.empty()) {
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+        return ans.empty() ? "/" : ans;
     }
-
-    string result = "";
-    for (auto &str : stack) result += "/" + str;
-    return result.empty() ? "/" : result;
-}
 };
