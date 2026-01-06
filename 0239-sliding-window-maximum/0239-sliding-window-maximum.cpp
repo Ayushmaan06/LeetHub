36ef9948c901ba338a24ca81ec8cdf9777c338a4
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-01-06 20:23:28 +0530

    Time: 1589 ms (5.1%), Space: 142.8 MB (20.23%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> pq; // Max-heap to store pairs of (value, index)
-        vector<int> result;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            // Push the current element along with its index onto the heap
-            pq.push({nums[i], i});
-
-            // Remove elements that are out of the current window
-            while (!pq.empty() && pq.top().second <= i - k) {
-                pq.pop();
-            }
-
-            // Record the maximum for the current window
-            if (i >= k - 1) {
-                result.push_back(pq.top().first);
-            }
+        int n = nums.size();
+        stack<int> st;
+        vector<int> ngr(n,-1),res,ngl;
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i])st.pop();
+            if(st.empty())ngr[i]=-1;
+            else ngr[i]=st.top();
+            st.push(i);
         }
-
-        return result;
+        // while(!st.empty())st.pop();
+        // for(int i = 0;i<n;i++){
+        //     while(!st.empty() && nums[st.top()]<nums[i])st.pop();
+        //     if(st.empty())ngl[i]=-1;
+        //     else ngl[i]=st.top();
+        //     st.push(i);
+        // }
+        for(int i = 0 ; i < n-k+1 ; i++){
+            int nr=ngr[i];
+            if(nr>=i+k || nr==-1){nr=i;res.push_back(nums[nr]);continue;}
+            int p;
+            while(nr!=-1 && nr<i+k)p=nr,nr=ngr[nr];
+            if(nr!=-1 && nr<i+k)res.push_back(nums[nr]);
+            else res.push_back(nums[p]);
+        }
+        return res;
     }
 };
