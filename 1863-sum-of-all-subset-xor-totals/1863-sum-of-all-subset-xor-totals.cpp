69ccc4809cb084615c3e45cb6586660b179900d8
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-01-21 22:28:11 +0530

    Time: 0 ms (100%), Space: 9.3 MB (18.34%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    int s=0;
-    void solve(vector<int>& v,int i,int cx){
-        if(i==v.size()){s+=cx;return;}
-        solve(v,i+1,cx);
-        cx=cx^v[i];
-        solve(v,i+1,cx);
-    }
     int subsetXORSum(vector<int>& nums) {
-        int cx=0;
-        solve(nums,0,cx);
-        return s;
+        int o = 0,n=nums.size();
+        for(int num : nums)o|=num;
+        int m=1<<(n-1);
+        return o*m;
     }
 };
