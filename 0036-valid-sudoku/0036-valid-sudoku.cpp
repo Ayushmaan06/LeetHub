4b012a2f3a751f1487056c4cf8154bdf28bcaf38
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-03-12 11:05:28 +0530

    Time: 9 ms (16.2%), Space: 24.3 MB (32.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& b) {
-        for(vector<char> v : b){
-            unordered_set<int> s;
-            for(char c : v){
-                if(isdigit(c)){
-                    if(s.count(c-'0'))return false;
-                    s.insert(c-'0');
-                }
-            }
-        }
+    bool isValidSudoku(vector<vector<char>>& board) {
         for(int i = 0 ; i < 9 ; i++){
-            unordered_set<int> s;
-            for(int j = 0 ; j < 9 ; j++){
-                char c = b[j][i];
-                if(isdigit(c)){
-                    if(s.count(c-'0'))return false;
-                    s.insert(c-'0');
-                }
+            unordered_set<char> r,c;
+            int rr=0,cc=0;
+            for(int j =0 ; j < 9 ; j++){
+                if(board[i][j]!='.')r.insert(board[i][j]),rr++;
+                if(board[j][i]!='.')c.insert(board[j][i]),cc++;
             }
+            if(rr!=r.size() || cc!=c.size())return false;
         }
-        for(int i = 0 ; i < 9 ; i+=3){
+        for(int i =0 ; i<9 ; i+=3){
             for(int j = 0 ; j < 9 ; j+=3){
-                unordered_set<int> s;
-                for(int x = i ; x < i+3 ; x++){
-                    for(int y = j ; y < j+3 ; y++){
-                        char c = b[x][y];
-                        if(isdigit(c)){
-                            if(s.count(c-'0'))return false;
-                            s.insert(c-'0');
-                        }
+                unordered_set<char> s;int c=0;
+                for(int k = i ; k < i+3 ; k++){
+                    for(int l = j ; l<j+3 ; l++){
+                         if(board[k][l]!='.')s.insert(board[k][l]),c++;
                     }
                 }
+                if(c!=s.size())return false;
             }
         }
         return true;
