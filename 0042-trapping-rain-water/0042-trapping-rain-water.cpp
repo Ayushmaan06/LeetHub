c65923d14626a6f35c01327a0fbed27382a04c19
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-03-14 20:21:26 +0530

    Time: 0 ms (100%), Space: 26.8 MB (42.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    void fillMaxHeight(vector<int>& heights, vector<int>& maxLeft, vector<int>& maxRight) {
-        int n = heights.size();
-        maxLeft[0] = heights[0];
-        for (int i = 1; i < n; i++) {
-            maxLeft[i] = max(maxLeft[i-1], heights[i]);
-        }
-        
-        maxRight[n-1] = heights[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            maxRight[i] = max(maxRight[i+1], heights[i]);
-        }
-    }
-    
     int trap(vector<int>& height) {
-        if (height.empty()) return 0;
-        
-        int n = height.size();
-        vector<int> leftGreater(n), rightGreater(n);
-        
-        fillMaxHeight(height, leftGreater, rightGreater);
-        
-        int totalWater = 0;
-        for (int i = 0; i < n; i++) {
-            int waterLevel = min(leftGreater[i], rightGreater[i]);
-            if (waterLevel > height[i]) {
-                totalWater += waterLevel - height[i];
-            }
+        int n = height.size(),h=-1;
+        vector<int> r(n,-1);
+        for(int i = n-1 ; i >= 0 ; i--){
+            r[i]=h;
+            h=max(h,height[i]);
+        }h=height[0];
+        int ans=0;
+        for(int i = 1 ; i < n ; i++){
+            if( h<=height[i]){h=max(h,height[i]);continue;}
+            ans+=max(0,min(r[i],h)-height[i]);
         }
-        
-        return totalWater;
+        return ans;
     }
 };
