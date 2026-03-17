1deb32c59d6ccdb6e0f62e6b3310f3984ae70c20
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-03-17 14:34:08 +0530

    Time: 2729 ms (5.02%), Space: 137.9 MB (94.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> ngr(n,-1),res,ngl;
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && nums[st.top()]<=nums[i])st.pop();
-            if(st.empty())ngr[i]=-1;
-            else ngr[i]=st.top();
+        stack<int> st;int n = nums.size();
+        vector<int> nge(n);
+        for(int i = n-1 ; i>=0 ; i--){
+            while(!st.empty() && nums[i] > nums[st.top()])st.pop();
+            if(st.empty())nge[i]=-1;
+            else nge[i]=st.top();
             st.push(i);
         }
-        // while(!st.empty())st.pop();
-        // for(int i = 0;i<n;i++){
-        //     while(!st.empty() && nums[st.top()]<nums[i])st.pop();
-        //     if(st.empty())ngl[i]=-1;
-        //     else ngl[i]=st.top();
-        //     st.push(i);
-        // }
-        for(int i = 0 ; i < n-k+1 ; i++){
-            int nr=ngr[i];
-            if(nr>=i+k || nr==-1){nr=i;res.push_back(nums[nr]);continue;}
-            int p;
-            while(nr!=-1 && nr<i+k)p=nr,nr=ngr[nr];
-            if(nr!=-1 && nr<i+k)res.push_back(nums[nr]);
-            else res.push_back(nums[p]);
+        vector<int> res(n-k+1);
+        for(int i = 0 ; i <= n-k ; i++ ){
+            if(nge[i]==-1)res[i]=nums[i];
+            else{
+                int j=i,p;
+                while(nge[j] < i+k && nge[j] != -1){
+                    j = nge[j];
+                }
+                res[i] = nums[j];
+            }
         }
         return res;
     }
