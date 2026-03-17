16e614201cac25976f60a51cbf8a3cf20db9cd4c
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-03-17 15:17:37 +0530

    Time: 6 ms (23.83%), Space: 23.6 MB (35.21%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,33 +1,34 @@
 class MinStack {
-private:
-    stack<int> s1, s2; // s1 is the main stack, s2 keeps track of the minimums
-
 public:
+    stack<int> s,ss;
     MinStack() {
         
     }
     
     void push(int val) {
-        s1.push(val); // Always push the value onto the main stack
-        if (s2.empty() || val <= s2.top()) {
-            // If s2 is empty or val is less than or equal to the current minimum, push it onto s2
-            s2.push(val);
-        }
+        s.push(val);
+        if(ss.empty() || ss.top()>=val)ss.push(val);
     }
     
     void pop() {
-        if (s1.top() == s2.top()) {
-            // If the top of s1 is the current minimum, pop it from s2 as well
-            s2.pop();
-        }
-        s1.pop(); // Always pop the top from the main stack
+        if(s.top()==ss.top())s.pop(),ss.pop();
+        else s.pop();
     }
     
     int top() {
-        return s1.top(); // Return the top of the main stack
+        return s.top();
     }
     
     int getMin() {
-        return s2.top(); // The top of s2 is the current minimum
+        return ss.top();
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
