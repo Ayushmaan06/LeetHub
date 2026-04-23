4651cb131e13b33edeb770330e5e11e4f333816d
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-04-23 10:56:32 +0530

    Time: 100 ms (30.5%), Space: 122.5 MB (31.75%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -1,24 +1,26 @@
 #define ll long long
+
 class Solution {
 public:
-    vector<long long> distance(vector<int>& nums) {
-        ll n = nums.size();
-        unordered_map<ll,vector<ll>> mp, mm;
-        for(ll i = 0 ; i < n ; i++)mp[nums[i]].push_back(i),mm[nums[i]].push_back(i);
-        for(auto& it : mp){
-            for(ll i = 1 ; i < it.second.size() ;i++)it.second[i]+=it.second[i-1];
+    vector<ll> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<ll> arr(n, 0);
+        unordered_map<int, ll> count, sum;
+        for (int i = 0; i < n; i++) {
+            int val = nums[i];
+            arr[i] += (ll)i * count[val] - sum[val];
+            count[val]++;
+            sum[val] += i;
         }
-        vector<ll> arr(n);
-        for(ll i = 0 ; i < n ; i++){
-            if(mp[nums[i]].size()==1){arr[i]=0;continue;}
-            ll j = lower_bound(mm[nums[i]].begin(), mm[nums[i]].end(),i)-mm[nums[i]].begin();
-            ll ans=0;
-            ll bs=mp[nums[i]][j]-i,
-                as=mp[nums[i]][ mp[nums[i]].size()-1]- mp[nums[i]][j],
-                bc=j*i,
-                ac=(mp[nums[i]].size()-1-j)*i;
-            arr[i]=(bc-bs)+(as-ac);
+        count.clear();
+        sum.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            int val = nums[i];
+            arr[i] += sum[val] - (ll)i * count[val];
+            count[val]++;
+            sum[val] += i;
         }
+
         return arr;
     }
 };
