afe638fe288e06f0dc8e33cbd081901f5773a106
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-05-20 16:25:04 +0530

    Time: 4 ms (57.31%), Space: 102.3 MB (34.28%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int ans=0;
-        int n = grid.size(),m=grid[0].size();
+    bool isc(int i , int n){
+        if(i>=0 && i< n)return true;
+        return false;
+    }
+    int islandPerimeter(vector<vector<int>>& g) {
+        vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int ans = 0, n = g.size(), m = g[0].size();
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j]==1){
-                    for(auto [r,c] : dir){
-                        int rr=r+i,cc=c+j;
-                        if(rr<0 || cc<0 ||rr>=n ||cc>=m )ans++;
-                        else{
-                            if(grid[rr][cc]==0)ans++;
-                        }
+                if(g[i][j]){
+                    for(auto[r,c] : dir){
+                        int rr = r+i , cc = c+j;
+                        if(isc(rr,n) && isc(cc,m) && !g[rr][cc])ans++;
+                        if(!isc(rr,n) || !isc(cc,m))ans++;
                     }
                 }
             }
