03712e5ee7b0c6032a0451ee54d5f862fc896219
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-06-01 14:46:25 +0530

    Time: 0 ms (100%), Space: 17 MB (7.87%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,11 +4,7 @@ public:
         sort(cost.rbegin(),cost.rend());
         long long ans = 0LL;
         if(cost.size()==1)return cost[0];
-        for(int i =0 ; i < cost.size();i++){
-            if(i%3==2)continue;
-            ans+=cost[i];
-        }
+        for(int i =0 ; i < cost.size();i++)if(i%3!=2) ans+=cost[i];
         return ans;
-
     }
 };
