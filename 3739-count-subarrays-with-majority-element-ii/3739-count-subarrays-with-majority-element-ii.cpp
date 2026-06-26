b4a3034be310ed9f478131cae145e60835687fc3
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-06-26 08:39:19 +0530

    Time: 77 ms (54.3%), Space: 104.9 MB (58.94%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,22 @@
+#define ll long long
+class Solution {
+public:
+        ll a[3]={0,1,2};
+    long long countMajoritySubarrays(vector<int>& nums, int x) {
+        ll n = nums.size();
+        vector<ll> pre(n+a[1],a[0]);
+        for(ll i = a[0] ; i <n ; i++)pre[i+a[1]]=pre[i]+(nums[i]==x ? a[1] : -a[1]);
+        vector<ll> al = pre;sort(al.begin(),al.end());al.erase(unique(al.begin(),al.end()),al.end());
+        auto id=[&](ll y){return int(lower_bound(al.begin(),al.end(),y)-al.begin())+a[1];};
+        ll m = al.size();
+        vector<ll> b(m+a[2],a[0]);
+        auto sum = [&](ll i ){ll s = 0LL; for(;i>a[0];i-=(i & -i))s+=b[i];return s;};
+        ll ans = 0LL;
+        for(ll i = a[0] ; i<=n ; i++){
+            ll idx = id(pre[i]);
+            ans+=sum(idx -a[1]);
+            for(;idx<=m; idx+=(idx & -idx))b[idx]++;
+        }
+        return ans;
+    }
+};
