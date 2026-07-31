371e8a8e55fba2a3ace9fd82f18c78ac8c27715a
Ayushmaan <142436337+Ayushmaan06@users.noreply.github.com>
2026-07-31 08:43:18 +0530

    Time: 0 ms (100%), Space: 21.6 MB (97.57%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void print(auto& c){
+        for(int x: c) cout<<x<<", "; 
+        cout<<endl;
+    }
+    int minimumPushes(string& word) {
+        int freq[26]={0};
+        for(char c: word) 
+            freq[c-'a']++;
+        sort(freq, freq+26, greater<int>());
+
+        int sz=0, push=1, ans=0;
+        for(; sz<26 && freq[sz]!=0; sz++){
+            if (sz>=8 && sz%8==0) push++;
+            ans+=freq[sz]*push;    
+        }
+        return ans;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
